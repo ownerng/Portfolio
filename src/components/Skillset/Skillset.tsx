3aa79a4a8aee76102b579fe3e6b1b6aeca982fd4
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2024-11-06 01:24:12 -0500

    finish icons

--- src/components/Skillset/Skillset.tsx
+++ src/components/Skillset/Skillset.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const skills = [
   "python", "fastapi", "django", "tailwindcss", "react",  
-  "postgresql", "nodejs", "Typescript", "git",
+  "postgresql", "nodejs", "typescript", "git",
   "flutter"
 ];
 
@@ -13,9 +13,9 @@ export const Skillset: React.FC = () => {
       <p className="text-gray-500 mb-8">
         Here are some of the cutting-edge technologies, programming languages and productivity tools that I use.
       </p>
-      <div className="grid grid-cols-4 gap-8">
+      <div className="grid grid-cols-5 grid-rows-2 gap-4 mx-16">
         {skills.map(skill => (
-          <div key={skill} className="grid">
+          <div key={skill} className="flex justify-center items-center">
             <img src={`/icons/${skill}.svg`} alt={skill} className="w-12 h-12"/>
             <h1 className='text-center font-medium text-2xl ml-2'>{skill}</h1>
           </div>
