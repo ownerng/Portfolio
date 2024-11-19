5755e0391d1462f6210c7d4767ad9e4054cb86d4
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2024-11-19 02:19:35 -0500

    no more commits on main

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,9 +5,9 @@ const Hero: React.FC = () => {
   return (
     <section className="flex flex-col items-center text-center py-16">
       <img src={Profile} alt="Descripción de la imagen" className="w-40 h-40 bg-gray-300 rounded-full mb-6 object-cover" />
-      <h1 className="text-4xl font-bold mb-4">Hi, I'm Luigi and I'm a Full Stack Developer</h1>
+      <h1 className="text-4xl font-bold mb-4">Hi, I'm Jhonnatan and I'm a Full Stack Developer</h1>
       <p className="text-gray-500 max-w-lg mb-6">
-        I’m currently based in the PH. I have 5 years of experience in Tech and Design and I am open for opportunities, collaborations and interesting projects. 100% Digital Native
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus voluptas eligendi consequuntur dolorum maiores, libero nesciunt voluptatum rerum, a fugiat, error repudiandae atque quibusdam. Mollitia corporis pariatur vero accusantium perspiciatis!
       </p>
       <div className="space-x-4">
         <button className="px-4 py-2 border rounded">Contact Me</button>
