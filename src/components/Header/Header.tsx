6c39ca874b622755644774d95462556745c73d20
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2024-11-29 02:39:29 -0500

    docker works

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 export const Header: React.FC = () => {
   return (
     <header className="flex justify-between items-center p-4">
-      <div className="text-2xl font-bold">LS</div>
+      <div className="text-2xl font-bold">Owner()</div>
       <nav className="space-x-4">
         <a href="#intro" className="hover:underline">Intro</a>
         <a href="#skillset" className="hover:underline">Skillset</a>
         <a href="#projects" className="hover:underline">Projects</a>
         <a href="#contact" className="hover:underline">Contact</a>
       </nav>
-      <div className="space-x-2">
-        <button>ENG | ESP</button>
-        <button>🌙</button>
+      <div className="flex">
+        <a href="https://github.com/ownerng" target="_blank"
+          rel="noopener noreferrer"><img src={`/icons/github.svg`} alt='Github' className="w-8 h-8" /></a>
       </div>
     </header>
   );
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       <img src={Profile} alt="Descripción de la imagen" className="w-40 h-40 bg-gray-300 rounded-full mb-6 object-cover" />
       <h1 className="text-4xl font-bold mb-4">Hi, I'm Jhonnatan and I'm a Full Stack Developer</h1>
       <p className="text-gray-500 max-w-lg mb-6">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus voluptas eligendi consequuntur dolorum maiores, libero nesciunt voluptatum rerum, a fugiat, error repudiandae atque quibusdam. Mollitia corporis pariatur vero accusantium perspiciatis!
+        docker
       </p>
       <div className="space-x-4">
         <button className="px-4 py-2 border rounded">Contact Me</button>
